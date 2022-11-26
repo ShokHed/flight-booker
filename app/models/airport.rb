643912143709da10a2f101587ec5417134a2855e
Matson Ovstedal <matsonov@gmail.com>
2022-11-25 19:59:50 -0800

    Create #name_city_state for select_collection text_method

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,4 +1,8 @@
 class Airport < ApplicationRecord
   has_many :arrivals, class_name: 'Flight', foreign_key: :destination_id
   has_many :departures, class_name: 'Flight', foreign_key: :origin_id
+
+  def name_city_state
+    "#{name} - #{city}, #{state}"
+  end
 end
