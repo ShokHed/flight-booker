f065f31a878395cf994225a8bf08e7925b1da924
Matson Ovstedal <matsonov@gmail.com>
2022-11-27 21:40:14 -0800

    Add param sanitization to controller, create _results partial

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,6 +3,13 @@ class FlightsController < ApplicationController
     @airports = Airport.all
     @flight_dates = Flight.today_or_after.distinct
 
+    @search_results = Flight.join(:origin, :destination).where(origin: { origin_id: params[:origin] },
+                                  destination: { desination_id: params[:desination] }).where( date: params[:flight_date])
+  end
 
+  private
+
+  def flight_params
+    params.require(:flight).permit(:origin_id, :destination_id, :flight_date)
   end
 end
