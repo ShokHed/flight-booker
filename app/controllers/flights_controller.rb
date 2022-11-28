d00c0c2f7a6f97afbe18fa60833d687b415f5b5c
Matson Ovstedal <matsonov@gmail.com>
2022-11-27 18:52:01 -0800

    Create date select dropdown, scope for current dates, controller query

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,7 @@
 class FlightsController < ApplicationController
   def index
     @airports = Airport.all
-    @flight_dates = Flight.where('start > ?', DateTime.now)
+    # @flight_dates = Flight.where('start > ?', DateTime.now)
+    @flight_dates = Flight.today_or_after
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,10 @@
 class Flight < ApplicationRecord
   belongs_to :origin, foreign_key: :origin_id, class_name: 'Airport'
   belongs_to :destination, foreign_key: :destination_id, class_name: 'Airport'
+
+  def flight_start_display
+    start.strftime("%m/%d/%Y")
+  end
+
+  scope :today_or_after, -> { where('start > ?', DateTime.now) }
 end
