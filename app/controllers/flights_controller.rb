2772cb266818c7febe357c98da0348f3392d57e5
Matson Ovstedal <matsonov@gmail.com>
2022-11-27 19:25:58 -0800

    Add distinct to flight date query

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,8 @@
 class FlightsController < ApplicationController
   def index
     @airports = Airport.all
-    # @flight_dates = Flight.where('start > ?', DateTime.now)
-    @flight_dates = Flight.today_or_after
+    @flight_dates = Flight.today_or_after.distinct
+
+
   end
 end
