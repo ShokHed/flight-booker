52bb2c328de9d6cca34fdc347ba4c639f42aa0ac
Matson Ovstedal <matsonov@gmail.com>
2022-12-07 11:45:49 -0800

    Change People table to Passengers

--- db/migrate/20221205032458_create_people.rb
+++ db/migrate/20221205032458_create_people.rb
@@ -1,6 +1,6 @@
 class CreatePeople < ActiveRecord::Migration[7.0]
   def change
-    create_table :people do |t|
+    create_table :passengers do |t|
       t.string :name
       t.string :email
       t.timestamps
--- db/migrate/20221205033642_add_through_association_foreign_keys_to_bookings.rb
+++ db/migrate/20221205033642_add_through_association_foreign_keys_to_bookings.rb
@@ -1,6 +1,6 @@
 class AddThroughAssociationForeignKeysToBookings < ActiveRecord::Migration[7.0]
   def change
     add_foreign_key :bookings, :flights, column: :flight_id, index: true
-    add_foreign_key :bookings, :people, column: :passenger_id, index: true
+    add_foreign_key :bookings, :passengers, column: :passenger_id, index: true
   end
 end
