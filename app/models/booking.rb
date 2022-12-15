a4381f4e16d3060ecf7febf41928f44acf543c9a
Matson Ovstedal <matsonov@gmail.com>
2022-12-14 18:02:36 -0800

    Accept nested attributes for passenger

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :flight, foreign_key: 'flight_id'
   belongs_to :passenger, foreign_key: 'passenger_id'
+
+  # accepts_nested_attributes_for :relationship, reject_if: ->(attributes){ attributes['name'].blank? }, allow_destroy: true
+  accepts_nested_attributes_for :passenger, allow_destroy: true
+
 end
