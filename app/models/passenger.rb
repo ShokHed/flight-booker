dc59f0b6a8407c1c5c76a3aebb406bf2dbd96af4
Matson Ovstedal <matsonov@gmail.com>
2022-12-14 20:24:34 -0800

    Booking has many passengers, accepts nested attributes for passengers

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,7 +1,7 @@
 class Passenger < ApplicationRecord
-  has_one :booking
+  belongs_to :booking
   # has_one :flight, through: :booking, source: :passenger, inverse_of: 'passenger'
 
-  has_one :flight, through: :booking#, foreign_key: 'passenger_id' #source: :passenger
+  has_one :flight, through: :booking # , foreign_key: 'passenger_id' #source: :passenger
 
 end
