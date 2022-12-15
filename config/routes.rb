385c237fa83abe4006bf901dd55004cf2612607d
Matson Ovstedal <matsonov@gmail.com>
2022-12-14 16:44:27 -0800

    Add view to list of allowed actions for bookings resource

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   #restrict to used methods
   resources :flights, only: [:index]
 
-  resources :bookings, only: [:new]
+  resources :bookings, only: [:new, :view]
 end
