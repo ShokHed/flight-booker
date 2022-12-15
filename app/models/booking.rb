51b5fe70935632f86a2a6fd141cef43fe5feb9d6
Matson Ovstedal <matsonov@gmail.com>
2022-12-14 18:12:18 -0800

    Singularize passengers -> passenger

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,6 @@ class Booking < ApplicationRecord
   belongs_to :passenger, foreign_key: 'passenger_id'
 
   # accepts_nested_attributes_for :relationship, reject_if: ->(attributes){ attributes['name'].blank? }, allow_destroy: true
-  accepts_nested_attributes_for :passengers, allow_destroy: true
+  accepts_nested_attributes_for :passenger, allow_destroy: true
 
 end
