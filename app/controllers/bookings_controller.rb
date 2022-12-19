14f59c36e6cc166539f081ec2a7e4f74528368be
Matson Ovstedal <matsonov@gmail.com>
2022-12-18 16:21:15 -0800

    Create nested passengers and save them

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,28 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
+    params[:number_of_passengers].to_i.times { @booking.passengers.build }
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+
+    respond_to do |format|
+      if @booking.save
+
+        # format.html { redirect_to post_url(@post), notice: "Post successfully created." }
+        format.html { redirect_to bookings_path(@booking), notice: "Booking successfully created." }
+        format.json { render :show, status: :created, location: @booking }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @booking.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:name, :email)
+    params.require(:booking).permit(:flight_id, passengers_attributes: [ :name, :email ])
   end
 end
