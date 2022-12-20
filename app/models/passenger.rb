32fca63c78aa679ecc3f7f8773724a11a888d39e
Matson Ovstedal <matsonov@gmail.com>
2022-12-20 11:57:53 -0800

    Add length validations to email and name

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,6 +2,6 @@ class Passenger < ApplicationRecord
   belongs_to :booking
   has_one :flight, through: :booking
 
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length: { in: 2..100 }
+  validates :email, presence: true, length: { in: 2..100 }
 end
