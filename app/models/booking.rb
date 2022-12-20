13a8825c2fddc5bbdd4d763163c94f89e7a7c54a
Matson Ovstedal <matsonov@gmail.com>
2022-12-20 11:14:34 -0800

    Set dependent: destroy option for passenger association

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :flight, foreign_key: 'flight_id'
-  has_many :passengers, inverse_of: :booking # , foreign_key: 'passenger_id'
+  has_many :passengers, inverse_of: :booking, dependent: :destroy # , foreign_key: 'passenger_id'
 
   # accepts_nested_attributes_for :relationship, reject_if: ->(attributes){ attributes['name'].blank? }, allow_destroy: true
   accepts_nested_attributes_for :passengers, allow_destroy: true, reject_if: :all_blank
