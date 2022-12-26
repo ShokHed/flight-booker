2b16a2163520371eb4211608c3c28e297ad03b89
Matson Ovstedal <matsonov@gmail.com>
2022-12-26 12:23:13 -0800

    Remove reject_if: :all_blank

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,6 @@ class Booking < ApplicationRecord
   belongs_to :flight, foreign_key: 'flight_id'
   has_many :passengers, inverse_of: :booking, dependent: :destroy
 
-  accepts_nested_attributes_for :passengers, reject_if: :all_blank, allow_destroy: true
-
+  accepts_nested_attributes_for :passengers, allow_destroy: true
   validates_associated :passengers
 end
