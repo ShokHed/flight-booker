99c98bf1b85c3e20fca80ba843456fa4587be4ec
Matson Ovstedal <matsonov@gmail.com>
2023-02-03 21:45:44 -0800

    Add ability to add/remove passenger fields via stimulus JS

---
+++ app/javascript/controllers/bookings_controller.js
@@ -0,0 +1,57 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "input","output" ]
+  static classes = [ "overLimit" ]
+  static values = { passengerCount: Number }
+  
+    connect() {
+      console.log(`init:passengerCountValue=${this.passengerCountValue}`)
+  }
+
+  removePassenger() {
+    console.log(`event.target.id=${event.target.id}`)
+    const passengerToRemove = document.getElementById(`passenger_${event.target.id}`)
+    passengerToRemove.remove()
+    this.passengerCountValue--
+    this.updateNumberOfPassengers()
+  }
+
+  addPassenger() {
+    const lastPassenger = document.getElementById(`passenger_${this.passengerCountValue - 1}`)
+
+    lastPassenger.after(passenger_fields.content.cloneNode(true))
+    this.passengerCountValue++
+    
+    this.setPassengerAttributeIndexes()
+    this.updateNumberOfPassengers()
+  }
+
+  updateNumberOfPassengers() {
+    const inputNumberPassengers = document.getElementById('booking_number_of_passengers')
+    inputNumberPassengers.setAttribute('value',`${this.passengerCountValue}`)
+    console.log(`add:passengerCountValue=${this.passengerCountValue}`)
+  }
+
+  setPassengerAttributeIndexes() {
+    const newPassengerDiv = document.getElementById(`passenger_new`)
+    
+    const nameLabel = newPassengerDiv.firstElementChild.firstElementChild
+    nameLabel.setAttribute("for",`booking_passengers_attributes_${this.passengerCountValue - 1}_name`)
+    const nameInput = nameLabel.nextElementSibling
+    nameInput.setAttribute("name",`booking[passengers_attributes][${this.passengerCountValue - 1}][name]`)
+    nameInput.setAttribute("id",`booking_passengers_attributes_${this.passengerCountValue - 1}_name`)
+
+    const emailLabel = newPassengerDiv.firstElementChild.nextElementSibling.firstElementChild
+    emailLabel.setAttribute("for",`booking_passengers_attributes_${this.passengerCountValue - 1}_email`)
+    const emailInput = emailLabel.nextElementSibling
+    emailInput.setAttribute("name",`booking[passengers_attributes][${this.passengerCountValue - 1}][email]`)
+    emailInput.setAttribute("id",`booking_passengers_attributes_${this.passengerCountValue - 1}_email`)
+
+    newPassengerDiv.setAttribute("id",`passenger_${this.passengerCountValue - 1}`)
+
+    const removeButton = document.getElementById('new')
+    removeButton.setAttribute("id",`${this.passengerCountValue - 1}`)
+
+  }
+}
