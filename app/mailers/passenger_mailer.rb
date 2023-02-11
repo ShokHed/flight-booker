bf1474e84cf6aaf2f8e9d45785b7f7ff43431d89
Matson Ovstedal <matsonov@gmail.com>
2023-02-11 15:56:37 -0800

    Create confirmation_email method

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,13 @@
 class PassengerMailer < ApplicationMailer
+
+  def confirmation_email
+    @passenger = params[:passenger]
+    @url = 'http://test.com'
+    mail(
+          to: @passenger.email, 
+          subject: 'Thank you for booking a trip with us!'
+        )
+    
+  end
+
 end
