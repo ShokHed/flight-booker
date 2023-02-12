5fdaf6b83f29f4bd1606780497c2997730495dee
Matson Ovstedal <matsonov@gmail.com>
2023-02-11 17:23:09 -0800

    Remove extraneous @url

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -2,12 +2,11 @@ class PassengerMailer < ApplicationMailer
 
   def confirmation_email
     @passenger = params[:passenger]
-    @url = 'http://test.com'
+
     mail(
           to: @passenger.email, 
           subject: 'Thank you for booking a trip with us!'
         )
-    
   end
 
 end
